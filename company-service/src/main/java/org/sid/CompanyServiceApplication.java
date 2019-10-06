585eb212b5c52d718c072a48c8662bf1562a62ef
jaafanii <medjaafani@gmail.com>
2019-10-06 21:00:33 +0100

    Initial commit

---
+++ company-service/src/main/java/org/sid/CompanyServiceApplication.java
@@ -0,0 +1,26 @@
+package org.sid;
+
+import org.sid.dao.CompanyRepository;
+import org.sid.entities.Company;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CompanyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(CompanyRepository companyRepository){
+        return  args->{
+            companyRepository.save(new Company(null,"c1",100));
+            companyRepository.save(new Company(null,"c2",200));
+            companyRepository.save(new Company(null,"c3",300));
+            companyRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
---
+++ company-service/src/main/java/org/sid/sec/SecurityConfig.java
@@ -0,0 +1,4 @@
+package org.sid.sec;
+
+public class SecurityConfig {
+}
